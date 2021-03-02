f182473585f0fa444afc775ff4b61391efafa442
Yoshi Yamaguchi <145104+ymotongpoo@users.noreply.github.com>
2021-03-02 23:29:07 +0900

    initial commit

---
+++ go.mod
@@ -0,0 +1,15 @@
+module github.com/ymotongpoo/exemplar-trace-sample-go
+
+go 1.16
+
+require (
+	cloud.google.com/go v0.78.0 // indirect
+	contrib.go.opencensus.io/exporter/stackdriver v0.13.5
+	go.opencensus.io v0.23.0
+	golang.org/x/net v0.0.0-20210226172049-e18ecbb05110 // indirect
+	golang.org/x/oauth2 v0.0.0-20210220000619-9bb904979d93 // indirect
+	golang.org/x/sync v0.0.0-20210220032951-036812b2e83c // indirect
+	golang.org/x/sys v0.0.0-20210301091718-77cc2087c03b // indirect
+	google.golang.org/genproto v0.0.0-20210226172003-ab064af71705 // indirect
+	google.golang.org/grpc v1.36.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,131 @@
+// Copyright 2021 Yoshi Yamaguchi
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"math/rand"
+	"os"
+	"time"
+
+	"cloud.google.com/go/compute/metadata"
+	"contrib.go.opencensus.io/exporter/stackdriver"
+	"go.opencensus.io/metric/metricdata"
+	"go.opencensus.io/stats"
+	"go.opencensus.io/stats/view"
+	"go.opencensus.io/trace"
+)
+
+const (
+	MeasureLatency = "task_latency_go"
+	LatencyUnit    = stats.UnitMilliseconds
+)
+
+var (
+	ProjectID   string
+	MLatency    *stats.Float64Measure
+	LatencyView *view.View
+)
+
+func initExporter() {
+	var err error
+	ProjectID, err = metadata.ProjectID()
+	if err != nil {
+		ProjectID = os.Getenv("GCP_PROJECT_ID")
+	}
+	if ProjectID == "" {
+		log.Fatalf("Specify GCP Project ID in $GCP_PROJECT_ID")
+	}
+
+	exporter, err := stackdriver.NewExporter(stackdriver.Options{
+		ProjectID: ProjectID,
+	})
+	if err != nil {
+		log.Fatalf("failed to create Cloud Ops exporter: %v", err)
+	}
+	if err = exporter.StartMetricsExporter(); err != nil {
+		log.Fatalf("Faield to start Cloud Monitoring exporter: %v", err)
+	}
+
+	MLatency = stats.Float64(MeasureLatency, "query latency", "ms")
+	LatencyView = &view.View{
+		Name:        MeasureLatency,
+		Measure:     MLatency,
+		Description: "the latency of root function per query",
+		Aggregation: view.Distribution(
+			[]float64{100.0, 200.0, 400.0, 1000.0, 2000.0, 4000.0}...,
+		),
+	}
+
+	trace.ApplyConfig(trace.Config{DefaultSampler: trace.AlwaysSample()})
+	trace.RegisterExporter(exporter)
+	view.RegisterExporter(exporter)
+	view.Register(LatencyView)
+}
+
+func main() {
+	initExporter()
+	log.Println("starting loop")
+	t := time.NewTicker(time.Second)
+	for range t.C {
+		log.Println("loop start")
+		Root()
+		log.Println("loop end")
+	}
+}
+
+func Root() {
+	ctx := context.Background()
+	ctx, span := trace.StartSpan(ctx, "root")
+	defer span.End()
+
+	start := time.Now().UnixNano()
+	tid := span.SpanContext().TraceID
+	sid := span.SpanContext().SpanID
+	spanName := fmt.Sprintf("projects/%v/traces/%v/spans/%v", ProjectID, tid, sid)
+	Foo(ctx)
+	end := time.Now().UnixNano()
+	log.Printf("span name: %v", spanName)
+	ms := (end - start) / (1000 * 1000)
+
+	measurements := stats.WithMeasurements(MLatency.M(float64(ms)))
+	attachments := stats.WithAttachments(metricdata.Attachments{
+		"@type":    "type.googleapis.com/google.monitoring.v3.SpanContext",
+		"spanName": spanName,
+	})
+	stats.RecordWithOptions(ctx, measurements, attachments)
+	return
+}
+
+func Foo(ctx context.Context) {
+	ctx, span := trace.StartSpan(ctx, "child_foo")
+	defer span.End()
+	ms := rand.Int63n(2000)
+	log.Printf("task foo blocked: %vms", ms)
+	span.AddAttributes(trace.Int64Attribute("foo_wait", ms))
+	Bar(ctx)
+	time.Sleep(time.Duration(ms) * time.Millisecond)
+}
+
+func Bar(ctx context.Context) {
+	ctx, span := trace.StartSpan(ctx, "child_bar")
+	defer span.End()
+	ms := rand.Int63n(1000)
+	log.Printf("task bar blocked: %vms", ms)
+	span.AddAttributes(trace.Int64Attribute("bar_wait", ms))
+	time.Sleep(time.Duration(ms) * time.Millisecond)
+}
